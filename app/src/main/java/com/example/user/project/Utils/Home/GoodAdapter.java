7c1eb9638886fe4515fedbea7f4ee08527ea0c38
Austin <37054635+huangtzuheng@users.noreply.github.com>
2019-05-05 23:06:29 +0800

    RecyclerView

---
+++ app/src/main/java/com/example/user/project/Utils/Home/GoodAdapter.java
@@ -0,0 +1,59 @@
+package com.example.user.project.Utils.Home;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.user.project.R;
+
+public class GoodAdapter extends RecyclerView.Adapter<GoodAdapter.GoodAdapterViewHolder> {
+    private String[] mGoodData;
+//    private int mTestNumbers;
+
+    public GoodAdapter(String[] mData) {
+        mGoodData = mData;
+    }
+
+    public class GoodAdapterViewHolder extends RecyclerView.ViewHolder {
+        public TextView mGoodTextView;
+        public LinearLayout mLinearLayout;
+
+        public GoodAdapterViewHolder(View view) {
+            super(view);
+            mGoodTextView = (TextView) view.findViewById(R.id.tv_good_data);
+            mLinearLayout = (LinearLayout) view.findViewById(R.id.ll);
+        }
+    }
+
+    @Override
+    public GoodAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.good_list;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new GoodAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(GoodAdapterViewHolder goodAdapterViewHolder, int position) {
+        String GoodForThisOne = mGoodData[position];
+        goodAdapterViewHolder.mGoodTextView.setText(GoodForThisOne);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mGoodData == null) return 0;
+        return mGoodData.length;
+    }
+
+    public void setGoodData(String[] goodData) {
+        mGoodData = goodData;
+        notifyDataSetChanged();
+    }
+}
