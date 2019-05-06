6f5398a0fdba4f1d2f49d68e541f37fde3aad354
kung10533034 <38273911+kung10533034@users.noreply.github.com>
2019-05-06 20:51:22 +0800

    RecyclerView

---
+++ app/src/main/java/com/example/user/project/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.user.project;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void sendString(View view) {
+
+        Intent intent = new Intent(this, Login2Activity.class);
+        EditText editText = (EditText) findViewById(R.id.input_email);
+        EditText editText1 = (EditText) findViewById(R.id.input_password);
+        String stringID = editText.getText().toString();
+        String seringPS = editText1.getText().toString();
+        if (login(stringID, seringPS) == true) {
+//            Toast t =new Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
+            intent.putExtra("userID", stringID);
+            intent.putExtra("userPS", seringPS);
+            startActivity(intent);
+        } else {
+//            AlertDialog ag = new AlertDialog.Builder(this)
+//                    .setTitle("WARING")
+//                    .setMessage("登入失敗")
+//                    .setPositiveButton("OK", null)
+//                    .show();
+            Intent intent2 = new Intent(this, LoginActivity.class);
+            startActivity(intent2);
+            //            intent.putExtra("userID","ERROR");
+            //        intent.putExtra("userPS","ERROR");
+            //        startActivity(intent);}
+
+
+        }
+    }
+    public boolean login(String id,String ps){
+        if (id.equals("ntust") && ps.equals("1234")){ //登入成功
+            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
+            return true;
+        }else {
+            AlertDialog ag = new AlertDialog.Builder(this)
+                    .setTitle("WARING")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK", null)
+                    .show();
+            return false;}
+    }
+}
