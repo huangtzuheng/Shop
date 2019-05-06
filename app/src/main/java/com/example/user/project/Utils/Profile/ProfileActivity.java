8555f4019415e1334f092b219f36be4e8d7feb25
Eugene0302 <37136606+Eugene0302@users.noreply.github.com>
2019-05-06 20:39:28 +0800

    RecyclerView

--- app/src/main/java/com/example/user/project/Utils/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/user/project/Utils/Profile/ProfileActivity.java
@@ -1,11 +1,18 @@
 package com.example.user.project.Utils.Profile;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.user.project.R;
 import com.example.user.project.Utils.Utils.BottomNavigationViewHelper;
@@ -15,13 +22,32 @@ public class ProfileActivity extends AppCompatActivity{
     private static final String TAG = "ProfileActivity";
     private static final int ACTIVITY_NUM = 4;
     private Context mContext = ProfileActivity.this;
+    private ProgressBar mProgressBar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG,"onCreate: starting.");
+        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+        mProgressBar.setVisibility(View.GONE);
 
         setupBottomNavigationView();
+        setupToolbar();
+    }
+    private void setupToolbar(){
+        Toolbar toolbar =(Toolbar)findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
+        profileMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to account setting.");
+                Intent intent = new Intent(mContext, AccountSettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
     private void setupBottomNavigationView()
     {
