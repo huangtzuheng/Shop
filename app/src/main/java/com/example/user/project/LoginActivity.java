0d21ca706a5b2cf65680ba86e18a9d78667e62a2
kung10533034 <38273911+kung10533034@users.noreply.github.com>
2019-05-06 21:15:24 +0800

    RecyclerView

--- app/src/main/java/com/example/user/project/LoginActivity.java
+++ app/src/main/java/com/example/user/project/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.user.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,27 +10,36 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText login_name,login_password;
     private int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        SharedPreferences remdname=getPreferences(LoginActivity.MODE_PRIVATE);
+        String name_str=remdname.getString("name", "");
+        String pass_str=remdname.getString("pass", "");
+        login_name.setText(name_str);
+        login_password.setText(pass_str);
     }
 
     public void sendString(View view) {
 
         Intent intent = new Intent(this, Login2Activity.class);
-        EditText editText = (EditText) findViewById(R.id.input_email);
-        EditText editText1 = (EditText) findViewById(R.id.input_password);
-        String stringID = editText.getText().toString();
-        String seringPS = editText1.getText().toString();
+        login_name = (EditText) findViewById(R.id.input_email);
+        login_password = (EditText) findViewById(R.id.input_password);
+        String stringID = login_name.getText().toString();
+        String seringPS = login_password.getText().toString();
         if (login(stringID, seringPS) == true) {
 //            Toast t =new Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
             intent.putExtra("userID", stringID);
             intent.putExtra("userPS", seringPS);
+            SharedPreferences remdname=getPreferences(LoginActivity.MODE_PRIVATE);
+            SharedPreferences.Editor edit=remdname.edit();
+            edit.putString("name", login_name.getText().toString());
+            edit.putString("pass", login_password.getText().toString());
+            edit.commit();
             startActivity(intent);
         } else {
 //            AlertDialog ag = new AlertDialog.Builder(this)
@@ -45,6 +55,16 @@ public class LoginActivity extends AppCompatActivity {
 
 
         }
+    }
+    protected void onPause(){
+        super.onPause();
+        SharedPreferences remdname=getPreferences(LoginActivity.MODE_PRIVATE);
+        SharedPreferences.Editor edit=remdname.edit();
+        edit.putString("name", login_name.getText().toString());
+        edit.putString("pass", login_password.getText().toString());
+        edit.commit();
+
+
     }
     public boolean login(String id,String ps){
         if (id.equals("ntust") && ps.equals("1234")){ //登入成功
