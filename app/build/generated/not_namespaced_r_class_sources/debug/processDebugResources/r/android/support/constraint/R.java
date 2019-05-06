73533bcded42b0dc3d16d90fdea52be17c28551a
Austin <37054635+huangtzuheng@users.noreply.github.com>
2019-05-06 18:33:11 +0800

    top bar adjust

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -80,13 +80,13 @@ public final class R {
         public static final int left = 0x7f080061;
         public static final int packed = 0x7f080076;
         public static final int parent = 0x7f080078;
-        public static final int percent = 0x7f08007c;
-        public static final int right = 0x7f080084;
-        public static final int spread = 0x7f0800a7;
-        public static final int spread_inside = 0x7f0800a8;
-        public static final int start = 0x7f0800ad;
-        public static final int top = 0x7f0800c3;
-        public static final int wrap = 0x7f0800d3;
+        public static final int percent = 0x7f08007b;
+        public static final int right = 0x7f080083;
+        public static final int spread = 0x7f0800a6;
+        public static final int spread_inside = 0x7f0800a7;
+        public static final int start = 0x7f0800ac;
+        public static final int top = 0x7f0800c2;
+        public static final int wrap = 0x7f0800d2;
     }
     public static final class styleable {
         private styleable() {}
