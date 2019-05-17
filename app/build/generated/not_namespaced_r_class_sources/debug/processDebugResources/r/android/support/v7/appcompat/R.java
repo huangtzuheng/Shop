a6171ca6d89e36dc63ec6f10af77bf63798d32b3
Austin <37054635+huangtzuheng@users.noreply.github.com>
2019-05-18 02:53:40 +0800

    2

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v7/appcompat/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v7/appcompat/R.java
@@ -711,19 +711,19 @@ public final class R {
         public static final int tag_transition_group = 0x7f0800d1;
         public static final int tag_unhandled_key_event_manager = 0x7f0800d2;
         public static final int tag_unhandled_key_listeners = 0x7f0800d3;
-        public static final int text = 0x7f0800d4;
-        public static final int text2 = 0x7f0800d5;
-        public static final int textSpacerNoButtons = 0x7f0800d9;
-        public static final int textSpacerNoTitle = 0x7f0800da;
-        public static final int time = 0x7f0800e2;
-        public static final int title = 0x7f0800e3;
-        public static final int titleDividerNoCustom = 0x7f0800e4;
-        public static final int title_template = 0x7f0800e5;
-        public static final int top = 0x7f0800e6;
-        public static final int topPanel = 0x7f0800e7;
-        public static final int uniform = 0x7f0800f0;
-        public static final int up = 0x7f0800f2;
-        public static final int wrap_content = 0x7f0800f9;
+        public static final int text = 0x7f0800d5;
+        public static final int text2 = 0x7f0800d6;
+        public static final int textSpacerNoButtons = 0x7f0800da;
+        public static final int textSpacerNoTitle = 0x7f0800db;
+        public static final int time = 0x7f0800e3;
+        public static final int title = 0x7f0800e4;
+        public static final int titleDividerNoCustom = 0x7f0800e5;
+        public static final int title_template = 0x7f0800e6;
+        public static final int top = 0x7f0800e7;
+        public static final int topPanel = 0x7f0800e8;
+        public static final int uniform = 0x7f0800f1;
+        public static final int up = 0x7f0800f3;
+        public static final int wrap_content = 0x7f0800fa;
     }
     public static final class integer {
         private integer() {}
